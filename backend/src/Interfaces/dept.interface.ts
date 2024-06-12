310dd32d999a83c02184f2134244018a3928675f
d-kingangi <itsronduncan@gmail.com>
2024-06-12 20:55:35 +0300

    backend: task interface + dept interface

---
+++ backend/src/Interfaces/dept.interface.ts
@@ -0,0 +1,5 @@
+export interface department {
+    name: string,
+    manager_id: string,
+    description: string
+}
---
+++ backend/src/Interfaces/task.interface.ts
@@ -0,0 +1,11 @@
+export interface task{
+    title: string,
+    description: string,
+    due_date: Date,
+    status: string,
+    assigned_to: string,
+    created_by: string,
+    estimated_effort: number,
+    completed_at: string,
+    labels: string,
+}
