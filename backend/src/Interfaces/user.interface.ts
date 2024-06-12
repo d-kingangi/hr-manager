d3bb0050e231617ddcb22f7fca7f516f2af8c3ba
d-kingangi <itsronduncan@gmail.com>
2024-06-12 20:43:09 +0300

    backend: user interface

---
+++ backend/src/Interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface user{
+    first_name: string,
+    last_name: string,
+    phone: string,
+    email: string,
+    roler: string,
+    dept_id:string,
+    password: string,
+    created_at: Date,
+    updated_at: Date
+}
