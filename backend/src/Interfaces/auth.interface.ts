8842b4399242118e915b32bf6fb9c72e87432e20
d-kingangi <itsronduncan@gmail.com>
2024-06-12 20:48:44 +0300

    backend: auth interface

---
+++ backend/src/Interfaces/auth.interface.ts
@@ -0,0 +1,14 @@
+export interface login_user_details{
+    user_id: string,
+    first_name: string,
+    last_name: string,
+    // phone: string,
+    email: string,
+    // role: string,
+    // dept_id: string,
+    password: string,
+    is_admin: number,
+    // created_at: string,
+    // updated_at: string,
+    // is_deleted: string,
+}
