7026812ec06b4706a0a25ab8208b355085ba6708
d-kingangi <itsronduncan@gmail.com>
2024-06-12 21:08:58 +0300

    backend: user validator

--- backend/src/Validators/user.validator.ts
+++ backend/src/Validators/user.validator.ts
@@ -11,3 +11,15 @@ export const register_user_schema = joi.object({
     created_at: joi.date(),
     updated_at: joi.date(),
 })
+
+export const update_user_schema = joi.object({
+    first_name: joi.string().required,
+    last_name: joi.string().required,
+    phone: joi.string().required,
+    email: joi.string().required,
+    role: joi.string().required,
+    dept_id: joi.string().required,
+    password: joi.string().required,
+    created_at: joi.date(),
+    updated_at: joi.date(),
+})
