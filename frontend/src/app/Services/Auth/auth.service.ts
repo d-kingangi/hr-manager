b745380d428c2787b7af4e6312f2463e849af1f7
d-kingangi <itsronduncan@gmail.com>
2024-06-13 19:18:38 +0300

    frontend: user + auth service

--- frontend/src/app/Services/Auth/auth.service.ts
+++ frontend/src/app/Services/Auth/auth.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { loginDetails, loginResponse } from '../../Interfaces/login.interface';
+import { user, userInfoResponse, allUsersResponse } from '../../Interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http:HttpClient) { }
+
+  readToken(token:string){
+    return this.http.get<{info:userInfoResponse}>(`${this.apiUrl}/auth/checkdetails`, {
+      headers: new HttpHeaders({
+        'Content-type': 'application/json',
+        'token': token
+      })
+    })
+  }
+
+  loginUser(details: loginDetails): Observable<loginResponse>{
+    return this.http.post<loginResponse>(`${this.apiUrl}/auth/login`, details)
+  }
+
+  checkUserDetails(token: string){
+    return this.http.get<userInfoResponse>(`${this.apiUrl}/auth/checkdetails`, {
+       headers: {
+        token
+      }
+    })
+  }
 }
--- frontend/src/app/Services/Users/user.service.ts
+++ frontend/src/app/Services/Users/user.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { user, userInfoResponse, allUsersResponse } from '../../Interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000'
+
+  getToken(){
+     const token = localStorage.getItem('token') as string
+     return token
+  }
+
+  constructor(private http:HttpClient) { }
+
+  register_user(user: user){
+    return this.http.post<userInfoResponse>(`${this.apiUrl}/user/register`, user)
+  }
+
+  get_all_users(){
+    return this.http.get<userInfoResponse>(`${this.apiUrl}/user/all`)
+  }
+
+  get_single_user(user_id: string){
+    return this.http.get<allUsersResponse>(`${this.apiUrl}/user/${user_id}`)
+  }
+
+  update_user(user_id: string, user: user){
+    return this.http.put<userInfoResponse>(`${this.apiUrl}/user/update/${user_id}`, user),{
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    }
+  }
+
+  delete_user(user_id: string){
+    return this.http.delete<userInfoResponse>(`${this.apiUrl}/user/delete/${user_id}`), {
+      headers: new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    }
+  }
 }
