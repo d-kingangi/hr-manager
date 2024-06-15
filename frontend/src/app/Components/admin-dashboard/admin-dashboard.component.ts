516dea997adb994953fb5f322369a837b6be80c8
d-kingangi <itsronduncan@gmail.com>
2024-06-15 10:58:31 +0300

    frontend: admin-dashboard

--- frontend/src/app/Components/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/Components/admin-dashboard/admin-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AdminSidebarComponent } from '../admin-sidebar/admin-sidebar.component';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ AdminSidebarComponent, RouterOutlet],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
