acd3fc2cb9fd8c1d5080369517d021a2cc1ae1d1
d-kingangi <itsronduncan@gmail.com>
2024-06-15 15:12:52 +0300

    frontend: manager dashboard

--- frontend/src/app/Components/manager-dashboard/manager-dashboard.component.ts
+++ frontend/src/app/Components/manager-dashboard/manager-dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ManagerSidebarComponent } from '../manager-sidebar/manager-sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-manager-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ RouterOutlet, ManagerSidebarComponent, NavbarComponent, FooterComponent ],
   templateUrl: './manager-dashboard.component.html',
   styleUrl: './manager-dashboard.component.css'
 })
+
 export class ManagerDashboardComponent {
 
 }
