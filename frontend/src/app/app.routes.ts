008c3083cf08f9b300a02c624d8e40f3468d57e2
d-kingangi <itsronduncan@gmail.com>
2024-06-15 16:06:05 +0300

    frontend: app.routes + update task component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,6 @@
-import { Routes } from '@angular/router';
-import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { authGuard } from './Guards/auth.guard';
 import { LandingComponent } from './Components/landing/landing.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
@@ -8,24 +9,49 @@ import { AdminManagersComponent } from './Components/admin-managers/admin-manage
 import { AdminDeptsComponent } from './Components/admin-depts/admin-depts.component';
 import { NewDeptComponent } from './Components/new-dept/new-dept.component';
 import { NewTaskComponent } from './Components/new-task/new-task.component';
+import { ManagerDashboardComponent } from './Components/manager-dashboard/manager-dashboard.component';
+import { ManagerProfileComponent } from './Components/manager-profile/manager-profile.component';
+import { ManagerTasksComponent } from './Components/manager-tasks/manager-tasks.component';
+import { EmployeeProfileComponent } from './Components/employee-profile/employee-profile.component';
+import { EmployeeDashboardComponent } from './Components/employee-dashboard/employee-dashboard.component';
+import { EmployeeTasksComponent } from './Components/employee-tasks/employee-tasks.component';
+import { UpdateProfileComponent } from './Components/update-profile/update-profile.component';
+import { UpdateTaskComponent } from './Components/update-task/update-task.component';
+import { AdminDashboardComponent } from './Components/admin-dashboard/admin-dashboard.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { AdminTasksComponent } from './Components/admin-tasks/admin-tasks.component';
+import { ManagerEmployeesComponent } from './Components/manager-employees/manager-employees.component';
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
     //admin routes
-    {path: 'employees', component: AdminEmployeesComponent},
-    {path: 'managers', component: AdminManagersComponent},
-    {path: 'depts', component: AdminDeptsComponent},
-    {path: 'new-dept', component: NewDeptComponent},
+    {path: 'admin/employees', component: AdminEmployeesComponent},
+    {path: 'admin/managers', component: AdminManagersComponent},
+    {path: 'admin/depts', component: AdminDeptsComponent},
+    {path: 'admin/depts/new', component: NewDeptComponent},
+    {path: 'admin/dash/:user_id', component: AdminDashboardComponent},
+    {path: 'admin/task/all', component: AdminTasksComponent},
     //manager routes
-    {path: 'new-task', component: NewTaskComponent}
-
+    {path: 'task/new', component: NewTaskComponent},
+    {path: 'dash/manager/:user_id', component: ManagerDashboardComponent},
+    {path: 'profile/manager/:user_id', component: ManagerProfileComponent},
+    {path: 'tasks/manager/:created_by', component: ManagerTasksComponent},
+    {path: 'dept/:dept_id/employees', component: ManagerEmployeesComponent},
+    //employee route
+    {path: 'profile/employee/:user_id', component: EmployeeProfileComponent},
+    {path: 'dash/employee/:user_id', component: EmployeeDashboardComponent},
+    {path: 'tasks/employee/:assigned_to', component: EmployeeTasksComponent},
+    //all
+    {path: 'profile/update/:user_id', component: UpdateProfileComponent},
+    {path: 'task/update/:task_id', component: UpdateTaskComponent},
+    {path: '**', component: NotFoundComponent}
 ];
 
-// @NgModule({
-//     imports: [RouterModule.forRoot(routes)],
-//     exports: [RouterModule]
-//   })
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
 
-//   export class AppRoutingModule { }
+  export class AppRoutingModule { }
