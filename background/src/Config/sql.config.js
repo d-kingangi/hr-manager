16428bfa3c3d743392cf9f78d5d8e1a95e8aa101
d-kingangi <itsronduncan@gmail.com>
2024-06-16 10:51:00 +0300

    background: sql config

---
+++ background/src/Config/sql.config.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import mssql from 'mssql'
+
+dotenv.config()
+
+export const sqlConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    server: process.env.SERVER,
+
+    pool:{
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+
+    options:{
+        encrypt: false,
+        trustServerCertificate: true
+    }
+}
