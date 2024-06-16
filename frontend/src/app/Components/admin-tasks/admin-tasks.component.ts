11b7df368dc523da22c801ebdcc67516ff1284d2
d-kingangi <itsronduncan@gmail.com>
2024-06-16 10:18:05 +0300

    frontend: admin task component

--- frontend/src/app/Components/admin-tasks/admin-tasks.component.ts
+++ frontend/src/app/Components/admin-tasks/admin-tasks.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { task, taskInfoResponse, allTasksResponse } from '../../Interfaces/task.interface';
+import { TaskService } from '../../Services/Tasks/task.service';
 
 @Component({
   selector: 'app-admin-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './admin-tasks.component.html',
   styleUrl: './admin-tasks.component.css'
 })
 export class AdminTasksComponent {
 
+  tasks: task[] = []
+
+  constructor(private TaskService: TaskService){
+    this.get_all_tasks()
+  }
+
+  get_all_tasks(){}
+
+  navigate_to_update_task(){}
 }
