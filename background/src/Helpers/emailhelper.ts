8484ac7d48fe15df3469042073c969a00c477cf1
d-kingangi <itsronduncan@gmail.com>
2024-06-16 10:55:51 +0300

    background: email helper + mail config

---
+++ background/src/Helpers/emailhelper.ts
@@ -0,0 +1,37 @@
+import nodemailer from 'nodemailer' 
+import dotenv from 'dotenv';
+import { mail_configs } from '../Interfaces/mail_config';
+
+
+dotenv.config();
+
+function createTransporter(config: mail_configs){
+    const transporter = nodemailer.createTransport()
+
+    return transporter
+}
+
+let configuration: mail_configs = ({
+    service : 'gmail',
+    host: 'smtp.gmail.com', 
+    port: 587,
+    requireTLS: true,
+    auth:{
+        user: "itsronduncan@gmail.com",
+        pass: "tiuzujzhrucqremx"
+    }
+})
+
+export const sendMail =async (messageOption: any) => {
+    const transporter = await createTransporter(configuration)
+
+    await transporter.verify()
+
+    await transporter.sendMail(messageOption, (error, info)=>{
+        if (error){
+            console.log(error);  
+        }else{
+            console.log(info.response);           
+        }
+    })
+}
