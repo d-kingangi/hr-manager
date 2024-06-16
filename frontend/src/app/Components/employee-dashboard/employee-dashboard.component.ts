205cf29162407ddefd652cca20b20a71305d6b8d
d-kingangi <itsronduncan@gmail.com>
2024-06-16 10:39:07 +0300

    frontend: employee dash component

--- frontend/src/app/Components/employee-dashboard/employee-dashboard.component.ts
+++ frontend/src/app/Components/employee-dashboard/employee-dashboard.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { EmployeeSidebarComponent } from '../employee-sidebar/employee-sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-employee-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ RouterOutlet, EmployeeSidebarComponent, NavbarComponent, FooterComponent],
   templateUrl: './employee-dashboard.component.html',
   styleUrl: './employee-dashboard.component.css'
 })
+
 export class EmployeeDashboardComponent {
 
 }
