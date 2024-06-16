31906b0988ba3cc953228612d783391981484134
d-kingangi <itsronduncan@gmail.com>
2024-06-16 09:39:38 +0300

    frontend: bug fixes

--- frontend/src/app/Components/not-found/not-found.component.ts
+++ frontend/src/app/Components/not-found/not-found.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [ NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
