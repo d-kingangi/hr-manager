e6e087e22696e96a50a8cd4a1202f7b6d571bd61
d-kingangi <itsronduncan@gmail.com>
2024-06-17 10:40:13 +0300

    background: server.ts

--- background/src/server.ts
+++ background/src/server.ts
@@ -1,2 +1,28 @@
 import express from 'express'
 import cron from 'node-cron'
+import { welcome_user, new_task, completed_task } from './Services/mail.service'
+
+const app = express()
+
+const run = async () => {
+    cron.schedule('*/5 * * * * *', async () => {
+        console.log('checking for a new Specialist');
+    
+        await welcome_user()
+    })
+}
+
+const run2 = async () => {
+    cron.schedule('*/5 * * * * *', async () => {
+        console.log('checking for a new task');
+
+        await new_task()
+    })
+}
+
+run()
+run2()
+
+app.listen(4201, () => {
+    console.log("server running ...");
+})
