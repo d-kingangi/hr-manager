dac696bcb2fb62abe18ed3a16608ac65e372304f
d-kingangi <itsronduncan@gmail.com>
2024-06-17 16:19:19 +0300

    frontend: navbar

--- frontend/src/app/Components/navbar/navbar.component.ts
+++ frontend/src/app/Components/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../Services/Auth/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ CommonModule, RouterLink, RouterOutlet,],
+  imports: [ CommonModule, RouterLink, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 
 export class NavbarComponent {
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private AuthService: AuthService){
+    this.check_auth()
+  }
 
   isLogged: boolean = false;
+  first_name!: string
+  last_name!: string
+  role!: string
   token = this.getToken()
 
   getToken(){
@@ -40,10 +47,26 @@ export class NavbarComponent {
     }
   }
 
+  check_auth(){
+    const token = this.getToken()
+    if(token != null){
+      this.AuthService.checkUserDetails(token).subscribe(res =>{
+        if(res.user[0]){
+          this.isLogged = true
+          this.first_name = res.user[0].first_name
+          this.last_name = res.user[0].last_name
+          this.role = res.user[0].role
+        }
+      })
+    } else{
+      this.isLogged = false
+    }
+  }
+
   logout(){
     if(typeof window !== 'undefined'){
       localStorage?.clear();
-      // this.router.navigate(['login'])
+      this.router.navigate(['/login'])
     } 
   }
 }
