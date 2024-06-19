f8baec2e484dc00f78166e4cc7386263a9f5f6ef
d-kingangi <itsronduncan@gmail.com>
2024-06-19 13:04:33 +0300

    frontend: admin-depts component

--- frontend/src/app/Components/admin-depts/admin-depts.component.ts
+++ frontend/src/app/Components/admin-depts/admin-depts.component.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, Router } from '@angular/router';
 import { department, departmentInfoResponse, allDepartmentsResponse } from '../../Interfaces/dept.interface';
 import { DeptService } from '../../Services/Depts/dept.service';
-import { UserService } from '../../Services/Users/user.service';
-import { AuthService } from '../../Services/Auth/auth.service';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 
@@ -19,13 +17,48 @@ import { FooterComponent } from '../footer/footer.component';
 export class AdminDeptsComponent {
 
   departments: department[]=[]
+  errorMsg!: string;
+  successMsg!: string;
+  errorDiv = false;
+  successDiv = false;
 
-  constructor(private UserService: UserService, private DeptService: DeptService, private AuthService: AuthService){
+  constructor( private DeptService: DeptService){
     this.get_all_depts()
   }
 
-  get_all_depts(){
-    this.DeptService.get_all_depts
+  get_all_depts() {
+    this.DeptService.get_all_depts().subscribe(
+      (response: allDepartmentsResponse) => {
+        console.log('Raw response:', response);
+        if (response.departments) {
+          this.departments = response.departments;
+          console.log('Departments fetched successfully:', this.departments);
+        } else {
+          console.error('Error fetching departments:', response.error);
+          this.displayErrors('An error occurred while fetching departments. Please try again later.');
+        }
+      },
+      (error) => {
+        console.error('Error fetching departments:', error);
+        this.displayErrors('An error occurred while fetching departments. Please try again later.');
+      }
+    );
+  }
+
+  displaySuccess(msg: string) {
+    this.successMsg = msg;
+    this.successDiv = true;
+    setTimeout(() => {
+      this.successDiv = false;
+    }, 2000);
+  }
+
+  displayErrors(msg: string) {
+    this.errorMsg = msg;
+    this.errorDiv = true;
+    setTimeout(() => {
+      this.errorDiv = false;
+    }, 2000);
   }
 
   navigate_to_single_dept(){}
