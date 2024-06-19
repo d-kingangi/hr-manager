5055cfb7a63d78cd656f27c3a914ea142f4f6d31
d-kingangi <itsronduncan@gmail.com>
2024-06-19 12:56:08 +0300

    frontend: auth.service fix

--- frontend/src/app/Services/Auth/auth.service.ts
+++ frontend/src/app/Services/Auth/auth.service.ts
@@ -2,36 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { loginDetails, loginResponse } from '../../Interfaces/login.interface';
-import { user, userInfoResponse, allUsersResponse } from '../../Interfaces/user.interface';
+import { userInfoResponse } from '../../Interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
 
   private apiUrl = 'http://localhost:3000';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  readToken(token:string){
-    return this.http.get<{info:userInfoResponse}>(`${this.apiUrl}/auth/checkdetails`, {
+  readToken(token: string): Observable<{ info: userInfoResponse }> {
+    return this.http.get<{ info: userInfoResponse }>(`${this.apiUrl}/auth/checkdetails`, {
       headers: new HttpHeaders({
-        'Content-type': 'application/json',
+        'Content-Type': 'application/json',
         'token': token
       })
-    })
+    });
   }
 
-  loginUser(details: loginDetails): Observable<loginResponse>{
-    return this.http.post<loginResponse>(`${this.apiUrl}/auth/login`, details)
+  loginUser(details: loginDetails): Observable<loginResponse> {
+    return this.http.post<loginResponse>(`${this.apiUrl}/auth/login`, details);
   }
 
-  checkUserDetails(token: string){
+  checkUserDetails(token: string): Observable<userInfoResponse> {
     return this.http.get<userInfoResponse>(`${this.apiUrl}/auth/checkdetails`, {
-       headers: {
-        token
-      }
-    })
+      headers: new HttpHeaders({
+        'token': token
+      })
+    });
   }
 }
