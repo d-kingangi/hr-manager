ee0785854c623e493d2563f787c7c88d32577b85
d-kingangi <itsronduncan@gmail.com>
2024-06-19 12:54:49 +0300

    frontend: dept.service ts fix

--- frontend/src/app/Services/Depts/dept.service.ts
+++ frontend/src/app/Services/Depts/dept.service.ts
@@ -2,52 +2,56 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { department, departmentInfoResponse, allDepartmentsResponse } from '../../Interfaces/dept.interface';
 import { user, userInfoResponse, allUsersResponse } from '../../Interfaces/user.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class DeptService {
-  private apiUrl = 'http://localhost:3000'
+  private apiUrl = 'http://localhost:3000';
 
-  getToken(){
-    const token = localStorage.getItem('token') as string
-    return token
+  constructor(private http: HttpClient) { }
+
+  getToken(): string {
+    const token = localStorage.getItem('token') || '';
+    return token;
   }
 
-  constructor(private http:HttpClient) { }
-
-  create_dept(department: department){
-    return this.http.post<departmentInfoResponse>(`${this.apiUrl}/dept/new`, department)
-  }
-
-  get_all_depts(){
-    return this.http.get<allDepartmentsResponse>(`${this.apiUrl}/dept/all`)
-  }
-
-  get_single_dept(dept_id: string){
-    return this.http.get<departmentInfoResponse>(`${this.apiUrl}/dept/${dept_id}`)
-  }
-
-  get_dept_employees(dept_id: string){
-    return this.http.get<allUsersResponse>(`${this.apiUrl}/dept/${dept_id}/employees`)
-  }
-
-  update_dept(dept_id: string, department: department){
-    return this.http.put<departmentInfoResponse>(`${this.apiUrl}/dept/update/${dept_id}`, department),{
+  create_dept(department: department): Observable<departmentInfoResponse> {
+    return this.http.post<departmentInfoResponse>(`${this.apiUrl}/dept/new`, department, {
       headers: new HttpHeaders({
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
         'Authorization': `Bearer ${this.getToken()}`
       })
-    }
+    });
   }
 
-  delete_dept(dept_id: string){
-    return this.http.delete<departmentInfoResponse>(`${this.apiUrl}/dept/delete/${dept_id}`),{
+  get_all_depts(): Observable<allDepartmentsResponse> {
+    return this.http.get<allDepartmentsResponse>(`${this.apiUrl}/dept/all`);
+  }
+
+  get_single_dept(dept_id: string): Observable<departmentInfoResponse> {
+    return this.http.get<departmentInfoResponse>(`${this.apiUrl}/dept/${dept_id}`);
+  }
+
+  get_dept_employees(dept_id: string): Observable<allUsersResponse> {
+    return this.http.get<allUsersResponse>(`${this.apiUrl}/dept/${dept_id}/employees`);
+  }
+
+  update_dept(dept_id: string, department: department): Observable<departmentInfoResponse> {
+    return this.http.put<departmentInfoResponse>(`${this.apiUrl}/dept/update/${dept_id}`, department, {
       headers: new HttpHeaders({
-        'content-type': 'application/json',
+        'Content-Type': 'application/json',
         'Authorization': `Bearer ${this.getToken()}`
       })
-    }
+    });
+  }
+
+  delete_dept(dept_id: string): Observable<departmentInfoResponse> {
+    return this.http.delete<departmentInfoResponse>(`${this.apiUrl}/dept/delete/${dept_id}`, {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.getToken()}`
+      })
+    });
   }
 }
