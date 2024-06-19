5a3622a1397e966c52474b4b8e20dae9dd922395
d-kingangi <itsronduncan@gmail.com>
2024-06-19 15:27:00 +0300

    frontend: manager-employees component

--- frontend/src/app/Components/manager-employees/manager-employees.component.ts
+++ frontend/src/app/Components/manager-employees/manager-employees.component.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../Services/Users/user.service';
+import { user, userInfoResponse, allUsersResponse } from '../../Interfaces/user.interface';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-manager-employees',
   standalone: true,
-  imports: [ RouterLink],
+  imports: [ RouterLink, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './manager-employees.component.html',
   styleUrl: './manager-employees.component.css'
 })
 
 export class ManagerEmployeesComponent {
+  users: user[] = [];
+  error: string = '';
+
+  constructor(private UserService: UserService){
+  }
+
+  ngOnInit(): void {
+    this.get_all_users();
+  }
+  
+  get_all_users() {
+    this.UserService.get_all_users().subscribe(
+      (response: allUsersResponse) => {
+        console.log('all users:', response)
+        this.users = response.users; 
+      },
+      (error) => {
+        console.error('Error fetching users:', error);
+      }
+    );
+  }
 
 }
