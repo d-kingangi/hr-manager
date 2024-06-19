8ec1dfb2b2a35f577d6d586cc844b626db3a7e2c
d-kingangi <itsronduncan@gmail.com>
2024-06-19 10:19:07 +0300

    frontend: landing fix

--- frontend/src/app/Components/landing/landing.component.ts
+++ frontend/src/app/Components/landing/landing.component.ts
@@ -23,33 +23,28 @@ export class LandingComponent {
 
   faqs: FaqItem[] = [
     { 
-      question: "What are the benefits of freelancing?", 
+      question: "What are the benefits of HR-Manager?", 
       answer: "Because Labour is borderless and global, anyone can apply for a job and get paid, no matter where our Freelancers and Customers are in the world, and regardless of whether they have access to banking services. .", 
       expanded: true
     },
     { 
-      question: "How can I earn on Labour?", 
+      question: "How can I earn on HR-Manager?", 
       answer: "Labour currently supports two major blockchains; Ethereum and BNB Chain. Freelancers and Customers can organise job payments in ETH, WBTC, TIME, and stablecoins", 
       expanded: false 
     },
     {
-      question: "How does a Labour Premium membership benefit Freelancers?",
+      question: "How does a HR-Manager Premium membership benefit Emloyees?",
       answer: "Freelancers are charged 10% in platform fees for each Job or Gig they complete. ",
       expanded: false
     },
     {
-      question: "How does a Labour Premium membership benefit Employers?",
+      question: "How does a HR-Manager Premium membership benefit Managers?",
       answer: "Customers receive a bonus in Labour tokens every time they make a payment to a Freelancer. This acts as an incentive for Customers to continue using Labour for all their hiring needs.",
       expanded: false
     }, 
     {
       question: "Can I apply for jobs without a Premium membership?",
       answer: "Absolutely! Labour’s cryptocurrency job market is open to all. ",
-      expanded: false
-    },
-    {
-      question: "What is ‘Job Mining’?",
-      answer: "Whenever a task is completed and a job payment is made, Labour converts its fee into LABOUR, the native token of the wider labour.tech ecosystem. ",
       expanded: false
     }
 
