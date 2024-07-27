838a823a0db9d3915dcfe89711c41c0640668e45
d-kingangi <itsronduncan@gmail.com>
2024-07-27 11:47:55 +0300

    test: user cont

--- backend/src/Controllers/__test__/user.controller.spec.ts
+++ backend/src/Controllers/__test__/user.controller.spec.ts
@@ -92,3 +92,138 @@ describe('Gets all users', ()=>{
 
     })
 })
+
+
+describe('Gets single user', ()=>{
+
+    let req: any
+    let res: any
+
+
+    beforeEach(()=>{
+        req = {
+            params: {
+                user_id: '353545-43495835-458347575', 
+            },
+        };
+        res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+        };
+    })
+
+    it('Successful fetch for a single member',async () => {
+        const mockedResult = [
+           //mock json result {}
+        ]
+    
+        const mockedInput = jest.fn().mockReturnThis() 
+    
+        const mockedExecute = jest.fn().mockResolvedValue({ recordset: mockedResult [0] })
+    
+        const mockedRequest ={
+            input: mockedInput,
+            execute: mockedExecute
+        }
+    
+        const mockedPool ={
+            request: jest.fn().mockReturnValue(mockedRequest)
+        }
+    
+        jest.spyOn(mssql, 'connect').mockResolvedValue(mockedPool as never)
+    
+        await get_single_user(req as any, res)
+    
+        // expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ error: "An error occurred while fetching member." });
+    })
+})
+
+
+describe('User updated successfully', ()=>{
+    let res: any
+    let req: any
+
+    beforeEach(()=>{
+        req={
+            params: {
+                user_id: '353545-43495835-458347575', 
+            }
+        }
+        res={
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
+        }
+    })
+
+    it('Successfully updates user',async () => {
+        const req={ 
+          body:{
+            //
+          }
+        }
+    
+        const mockedInput = jest.fn().mockReturnThis() 
+    
+        const mockedExecute = jest.fn().mockResolvedValue({rowsAffected: [1]})
+    
+        const mockedRequest ={
+           input: mockedInput,
+           execute: mockedExecute
+        }
+    
+        const mockedPool ={
+          request: jest.fn().mockReturnValue(mockedRequest)
+        }
+    
+        await update_user(req as any, res);
+    
+        jest.spyOn(mssql, 'connect').mockResolvedValue(mockedPool as never)
+    
+        expect(res.json).toHaveBeenCalledWith({message: "User updated successfully"})
+    
+    })
+})
+
+
+describe('Account deleted successfully', ()=>{
+
+    let res: any
+    let req: any
+
+    beforeEach(()=>{
+        req={
+            params:{
+                user_id: '353545-43495835-458347575'
+            }
+        }
+        res= {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis
+        }
+    })
+
+    it('Successfully deletes user',async () => {
+        const req={ body:{}}
+        
+        const mockedInput = jest.fn().mockReturnThis()
+  
+        const mockedExecute = jest.fn().mockResolvedValue({rowsAffected: [1]})
+  
+        const mockedRequest ={
+            input: mockedInput,
+            execute: mockedExecute
+        }
+  
+        const mockedPool ={
+            request: jest.fn().mockReturnValue(mockedRequest)
+        }
+  
+        jest.spyOn(mssql, 'connect').mockResolvedValue(mockedPool as never)
+  
+        await delete_user(req as any, res)
+  
+        // expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({message: "Account deleted successfully"})
+    })
+})
